df0112b591759a167f4cbdbf724c6c1b4a3602f6
Jimmy Klein <jimmy.klein4@gmail.com>
2015-09-19 17:27:17 -0400

    asdf

--- app/src/main/java/org/tudev/bigred/groupsound/DeviceDetailFragment.java
+++ app/src/main/java/org/tudev/bigred/groupsound/DeviceDetailFragment.java
@@ -256,7 +256,7 @@ public class DeviceDetailFragment extends Fragment implements ConnectionInfoList
      * A simple server socket that accepts connection and writes some data on
      * the stream.
      */
-    public static class FileServerAsyncTask extends AsyncTask<Socket, Void, Void> {
+    public static class FileServerAsyncTask extends AsyncTask<Socket, Void, String> {
 
         private Context context;
         private TextView statusText;
@@ -294,8 +294,32 @@ public class DeviceDetailFragment extends Fragment implements ConnectionInfoList
             }
         }
 
+        @Override
         protected String doInBackground(Void... params){
-            return null;
+            try {
+                ServerSocket serverSocket = new ServerSocket(8988);
+                serverSocket.setReuseAddress(true);
+                Log.d(TAG, "Server: Socket opened");
+                Socket client = serverSocket.accept();
+                Log.d(TAG, "Server: connection done");
+                final File f = new File(Environment.getExternalStorageDirectory() + "/"
+                        + context.getPackageName() + "/wifip2pshared-" + System.currentTimeMillis()
+                        + ".mp4");
+
+                File dirs = new File(f.getParent());
+                if (!dirs.exists())
+                    dirs.mkdirs();
+                f.createNewFile();
+
+                Log.d(TAG, "server: copying files " + f.toString());
+                InputStream inputstream = client.getInputStream();
+                copyFile(inputstream, new FileOutputStream(f));
+                serverSocket.close();
+                return f.getAbsolutePath();
+            } catch (IOException e) {
+                Log.e(TAG, e.getMessage());
+                return null;
+            }
         }
 
         /*
@@ -303,7 +327,7 @@ public class DeviceDetailFragment extends Fragment implements ConnectionInfoList
          * @see android.os.AsyncTask#onPostExecute(java.lang.Object)
          */
         @Override
-        protected void onPostExecute() {
+        protected void onPostExecute(String result) {
             if (result != null) {
                 statusText.setText("File copied - " + result);
                 Intent intent = new Intent();
