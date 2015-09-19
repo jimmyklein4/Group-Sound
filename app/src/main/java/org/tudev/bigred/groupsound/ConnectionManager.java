167abd560e9ab6624cdde2cd4e51e63773683f19
Jimmy Klein <jimmy.klein4@gmail.com>
2015-09-19 02:33:59 -0400

    Added some files

---
+++ app/src/main/java/org/tudev/bigred/groupsound/ConnectionManager.java
@@ -0,0 +1,200 @@
+package org.tudev.bigred.groupsound;
+
+import android.util.Log;
+
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ConnectionManager{
+
+    private Socket server;
+    private ServerSocket serverSocket;
+    private static String TAG = "ConnectionManager";
+    private ArrayList<Socket> clients;
+    private long startTime, endTime;
+    private boolean isFirst = false;
+    private byte[] data;
+    //We are the client
+    public ConnectionManager(final InetAddress host){
+        data = new byte[64];
+        data = getByteArray();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    server = new Socket(host, 8988);
+                    server.setReuseAddress(true);
+                } catch (java.io.IOException e) {
+                    Log.d(TAG, e.toString());
+                }
+            }
+        }).start();
+
+    }
+
+    //We are the host
+    public ConnectionManager(){
+        data = new byte[64];
+        data = getByteArray();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    serverSocket = new ServerSocket(8988);
+                    serverSocket.setReuseAddress(true);
+                }catch(java.io.IOException e){
+                    Log.d(TAG, e.toString());
+                }
+            }
+        }).start();
+
+    }
+
+    public void setStartTime(long startTime){
+        this.startTime = startTime;
+    }
+
+    public void setEndTime(long endTime){
+        this.endTime = endTime;
+    }
+
+    public void setIsFirst(boolean isFirst){
+        this.isFirst = isFirst;
+    }
+
+    public void serverConnect(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    if(clients==null){
+                        clients = new ArrayList<>();
+                    }
+                    Socket socket = serverSocket.accept();
+                    clients.add(socket);
+                    if(clients.size()>0) {
+                        serverListen(clients.get(clients.size() - 1));
+                    }
+                }catch(java.io.IOException e){
+                    Log.d(TAG, e.toString());
+                }
+            }
+        }).start();
+    }
+    //The clients will listen for traffic
+    public void clientListen(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int i=0;
+                while (i<25){
+                    try {
+                        InputStream stream = server.getInputStream();
+                        int count = stream.read(data);
+                        if (count == 64) {
+                            if (isFirst) {
+                                Log.d(TAG, " " + (System.currentTimeMillis() - startTime));
+                                i++;
+                                setStartTime(System.currentTimeMillis());
+                                clientSendPing();
+                            } else {
+                                clientSendPing();
+                            }
+                        }
+                    } catch (java.io.IOException e) {
+                        Log.d(TAG, e.toString());
+                    }
+                }
+            }
+        }).start();
+    }
+    //Server will listen for traffic
+    public void serverListen(final Socket socket){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                boolean isListening = true;
+                while(isListening) {
+                    try {
+                        InputStream stream = socket.getInputStream();
+                        int count = stream.read(data);
+                        if(count==64){
+                            serverSendPing(clients.get(0));
+                            /*
+                            This is for 3 total phones
+                            if ((socket == clients.get(0)) && (clients.size() > 1)) {
+                                serverSendPing(clients.get(1));
+                            } else if ((socket == clients.get(1)) && (clients.size() > 1)) {
+                                serverSendPing(clients.get(0));
+                            }
+                            */
+                        }
+                    } catch (java.io.IOException e) {
+                        Log.d(TAG, e.toString() + "serverlisten");
+                    }
+                }
+            }
+        }).start();
+    }
+
+    public void closeClientConnection(){
+        try{
+            if(server!=null){
+                server.close();
+            }
+        }catch(java.io.IOException e){
+            Log.d(TAG, e.toString());
+        }
+    }
+
+    public void closeServerConnection(){
+        try {
+            if(serverSocket!=null){
+                serverSocket.close();
+            }
+        }catch(java.io.IOException e){
+            Log.d(TAG, e.toString());
+        }
+    }
+
+    public void clientSendPing(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    DataOutputStream dataOutputStream = new DataOutputStream(server.getOutputStream());
+                    dataOutputStream.write(data,0,64);
+                }catch(java.io.IOException e){
+                    Log.d(TAG, e.toString());
+                }
+            }
+        }).start();
+    }
+
+    private void serverSendPing(final Socket otherClient){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    DataOutputStream dataOutputStream = new DataOutputStream(otherClient.getOutputStream());
+                    dataOutputStream.write(data,0,64);
+                }catch(java.io.IOException e){
+                    Log.d(TAG, e.toString() + "serversendping");
+                }
+            }
+        }).start();
+    }
+
+    private byte[] getByteArray(){
+        byte[] tmp = new byte[64];
+
+        for(int i=0;i<64;i++){
+            tmp[i]=0xF;
+        }
+        return tmp;
+    }
+}
---
+++ app/src/main/java/org/tudev/bigred/groupsound/DeviceDetailFragment.java
@@ -0,0 +1,196 @@
+package org.tudev.bigred.groupsound;
+
+
+import android.app.Fragment;
+import android.app.ProgressDialog;
+
+import android.net.wifi.WpsInfo;
+import android.net.wifi.p2p.WifiP2pConfig;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pGroup;
+import android.net.wifi.p2p.WifiP2pManager.GroupInfoListener;
+import android.net.wifi.p2p.WifiP2pInfo;
+import android.net.wifi.p2p.WifiP2pManager.ConnectionInfoListener;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.jimmyklein.wifidirecttest2.DeviceListFragment.DeviceActionListener;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * A fragment that manages a particular peer and allows interaction with device
+ * i.e. setting up network connection and transferring data.
+ */
+public class DeviceDetailFragment extends Fragment implements ConnectionInfoListener, GroupInfoListener {
+
+    public static final String TAG = "DeviceDetailFragment";
+    private View mContentView = null;
+    private WifiP2pDevice device;
+    private WifiP2pGroup group;
+    private WifiP2pInfo info;
+    private ConnectionManager server;
+    private ConnectionManager client;
+    ProgressDialog progressDialog = null;
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        mContentView = inflater.inflate(R.layout.device_detail, null);
+        mContentView.findViewById(R.id.btn_connect).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                WifiP2pConfig config = new WifiP2pConfig();
+                config.deviceAddress = device.deviceAddress;
+                config.wps.setup = WpsInfo.PBC;
+                if (progressDialog != null && progressDialog.isShowing()) {
+                    progressDialog.dismiss();
+                }
+                progressDialog = ProgressDialog.show(getActivity(), "Press back to cancel",
+                        "Connecting to :" + device.deviceAddress, true, true
+                );
+                ((DeviceActionListener) getActivity()).connect(config);
+
+            }
+        });
+
+        mContentView.findViewById(R.id.btn_disconnect).setOnClickListener(
+                new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        if(server!=null){
+                            server.closeServerConnection();
+                        } else if(client!=null){
+                            client.closeClientConnection();
+                        }
+                        ((DeviceActionListener) getActivity()).disconnect();
+                    }
+                });
+
+        mContentView.findViewById(R.id.btn_start_client).setOnClickListener(
+                new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        client.setStartTime(System.currentTimeMillis());
+                        client.setIsFirst(true);
+                        client.clientSendPing();
+                        client.clientListen();
+                    }
+                });
+        mContentView.findViewById(R.id.btn_listen).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        client.clientListen();
+                    }
+                }
+        );
+
+        return mContentView;
+    }
+
+    @Override
+    public void onConnectionInfoAvailable(final WifiP2pInfo info)
+    {
+        if (progressDialog != null && progressDialog.isShowing()) {
+            progressDialog.dismiss();
+        }
+        this.info = info;
+        this.getView().setVisibility(View.VISIBLE);
+
+        // The owner IP is now known.
+        TextView view = (TextView) mContentView.findViewById(R.id.group_owner);
+        view.setText(getResources().getString(R.string.group_owner_text) + ((info.isGroupOwner) ? getResources().getString(R.string.yes)
+                : getResources().getString(R.string.no)));
+
+        // InetAddress from WifiP2pInfo struct.
+        view = (TextView) mContentView.findViewById(R.id.device_info);
+
+        if (info.groupFormed && info.isGroupOwner) {
+            if(server==null) {
+                server = new ConnectionManager();
+            }
+            server.serverConnect();
+        } else if (info.groupFormed) {
+            mContentView.findViewById(R.id.btn_start_client).setVisibility(View.VISIBLE);
+            mContentView.findViewById(R.id.btn_listen).setVisibility(View.VISIBLE);
+            client = new ConnectionManager(info.groupOwnerAddress);
+            mContentView.findViewById(R.id.btn_connect).setVisibility(View.GONE);
+            //InetAddress go = connectionManager.listen();
+        }
+
+        // hide the connect button
+    }
+
+    public void onGroupInfoAvailable(final WifiP2pGroup group)
+    {
+        this.group = group;
+    }
+
+    /**
+     * Updates the UI with device data
+     *
+     * @param device the device to be displayed
+     */
+    public void showDetails(WifiP2pDevice device) {
+        this.device = device;
+        this.getView().setVisibility(View.VISIBLE);
+        TextView view = (TextView) mContentView.findViewById(R.id.device_address);
+        view.setText(device.deviceAddress);
+        view = (TextView) mContentView.findViewById(R.id.device_info);
+        view.setText(device.toString());
+
+    }
+
+    /**
+     * Clears the UI fields after a disconnect or direct mode disable operation.
+     */
+    public void resetViews() {
+        mContentView.findViewById(R.id.btn_connect).setVisibility(View.VISIBLE);
+        TextView view = (TextView) mContentView.findViewById(R.id.device_address);
+        view.setText(R.string.empty);
+        view = (TextView) mContentView.findViewById(R.id.device_info);
+        view.setText(R.string.empty);
+        view = (TextView) mContentView.findViewById(R.id.group_owner);
+        view.setText(R.string.empty);
+        view = (TextView) mContentView.findViewById(R.id.status_text);
+        view.setText(R.string.empty);
+        mContentView.findViewById(R.id.btn_start_client).setVisibility(View.GONE);
+        this.getView().setVisibility(View.GONE);
+    }
+
+    /**
+     * A simple server socket that accepts connection and writes some data on
+     * the stream.
+     */
+
+    public static boolean copyFile(InputStream inputStream, OutputStream out) {
+        byte buf[] = new byte[1024];
+        int len;
+        try {
+            while ((len = inputStream.read(buf)) != -1) {
+                out.write(buf, 0, len);
+            }
+            out.close();
+            inputStream.close();
+        } catch (IOException e) {
+            Log.d(TAG, e.toString());
+            return false;
+        }
+        return true;
+    }
+}
