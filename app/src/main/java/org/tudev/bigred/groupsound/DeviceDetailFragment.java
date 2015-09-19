b307d46205c1f0e45ff2f7295a6b16bf827872e4
Jimmy Klein <jimmy.klein4@gmail.com>
2015-09-19 02:44:03 -0400

    Added files

--- app/src/main/java/org/tudev/bigred/groupsound/DeviceDetailFragment.java
+++ app/src/main/java/org/tudev/bigred/groupsound/DeviceDetailFragment.java
@@ -18,7 +18,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.example.jimmyklein.wifidirecttest2.DeviceListFragment.DeviceActionListener;
+import org.tudev.bigred.groupsound.DeviceListFragment.DeviceActionListener;
 
 import java.io.IOException;
 import java.io.InputStream;
